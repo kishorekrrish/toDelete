f58ca6cced056140f580a0f49a8572e6a5263b7a
kishorekrrish <57085416+kishorekrrish@users.noreply.github.com>
2020-06-01 10:59:46 +0530

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -1,13 +1,23 @@
-const path = require('path');
+// Simple Express server setup to serve the build output
+const compression = require('compression');
+const helmet = require('helmet');
 const express = require('express');
-
-const customServer = require(path.resolve('./src/server/'));
+const path = require('path');
 
 const app = express();
+app.use(helmet());
+app.use(compression());
 
-customServer(app);
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const DIST_DIR = './dist';
 
-app.listen(3002, () => {
-    // eslint-disable-next-line no-console
-    console.log('Yay, local server started');
+app.use(express.static(DIST_DIR));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
 });
+
+app.listen(PORT, () =>
+    console.log(`✅  Server started: http://${HOST}:${PORT}`)
+);
